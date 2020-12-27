6c973a793959088a98bc1cd28f14d47c71aa9349
Aman Aggarwal <aman.aggarwal195@gmail.com>
2020-12-27 23:48:10 +0530

    Item detail controller updated

--- assignments/module4-solution/src/components/itemDetail.controller.js
+++ assignments/module4-solution/src/components/itemDetail.controller.js
@@ -1,8 +1,8 @@
 angular.module('MenuApp')
 .controller('ItemDetailController',ItemDetailController);
 
-ItemDetailController.$inject=['$stateParams','items'];
-function ItemDetailController($stateParams,items){
+ItemDetailController.$inject=['items'];
+function ItemDetailController(items){
 	var itemDetail=this;
 	itemDetail.items=items;
 }
