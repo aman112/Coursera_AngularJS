bb5e3f0a77f5a8815c6718de9fafb552490f7038
Aman Aggarwal <aman.aggarwal195@gmail.com>
2020-12-27 22:27:35 +0530

    Item detail controller name fixed

--- assignments/module4-solution/src/components/itemDetail.controller.js
+++ assignments/module4-solution/src/components/itemDetail.controller.js
@@ -2,7 +2,7 @@ angular.module('MenuApp')
 .controller('ItemDetailController',ItemDetailController);
 
 ItemDetailController.$inject=['$stateParams','categories'];
-function MenuCategoriesController($stateParams,categories){
+function ItemDetailController($stateParams,categories){
 	var itemDetail=this;
 	itemDetail.categories=categories;
 	var item = categories[$stateParams.itemId];
