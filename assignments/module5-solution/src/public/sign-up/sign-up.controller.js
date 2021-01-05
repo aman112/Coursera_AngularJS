5c5504767925e4c11138ec7b048c6ec8df4fe680
Aman Aggarwal <aman.aggarwal195@gmail.com>
2021-01-06 02:57:28 +0530

    Sign Up Page update 1

--- assignments/module5-solution/src/public/sign-up/sign-up.controller.js
+++ assignments/module5-solution/src/public/sign-up/sign-up.controller.js
@@ -4,8 +4,26 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = [];
-function SignUpController() {
+SignUpController.$inject = ['$http'];
+function SignUpController($http) {
   var $ctrl = this;
+  
+  $ctrl.submit=function(){
+	alert('Submit called...');
+	  $http({
+		  method:"GET",
+		  url:ApiPath+"/menu_items/"+$ctrl.user.favDishName+".json"
+	  })
+	  .then(function(response){
+		  console.log(response);
+		  if(response.status=="500" && response.error==="Internal Server Error"){
+			  $ctrl.itemFound=false;
+		  }
+		  else{
+			  console.log("service add called...");
+			  $ctrl.itemFound=true;
+		  }
+	  });
+	}
 }
 })();
