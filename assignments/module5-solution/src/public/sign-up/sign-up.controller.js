5a23fc93ae68d57597e11c5ce001c9cbb181adc3
Aman Aggarwal <aman.aggarwal195@gmail.com>
2021-01-08 02:46:45 +0530

    Sign Up Fixes

--- assignments/module5-solution/src/public/sign-up/sign-up.controller.js
+++ assignments/module5-solution/src/public/sign-up/sign-up.controller.js
@@ -4,32 +4,27 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['$http','ApiPath','MenuService','RegUserInfoService'];
-function SignUpController($http,ApiPath,MenuService,RegUserInfoService) {
+SignUpController.$inject = ['ApiPath','MenuService','RegUserInfoService'];
+function SignUpController(ApiPath,MenuService,RegUserInfoService) {
 	var $ctrl = this;
 	$ctrl.itemFetchCalled=false;
 	$ctrl.submit=function(){
-	  /* $http({
-		  method:"GET",
-		  url:ApiPath+"/menu_items/"+$ctrl.user.favDishName+".json"
-	  }) */
-		/* if(undefined!==$ctrl.user.favDishName && null!==$ctrl.user.favDishName && $ctrl.user.favDishName!==""){ */
-			$ctrl.itemFetchCalled=true;
-			MenuService.getMenuItemsByShortName($ctrl.user.favDishName)
-			.then(function(response){
-				if(!RegUserInfoService.checkInfo($ctrl.user.email)){
-					$ctrl.user.menuItem=response.data;
-					RegUserInfoService.savePreferences($ctrl.user);
-					$ctrl.itemFound=true;
-					$ctrl.user={};
-					$ctrl.itemFetchCalled=false;
-				}
-				else{
-					alert("This email Id is already been registered.");
-				}
-			},function(error){
-				$ctrl.itemFound=false;
-			});
+		$ctrl.itemFetchCalled=true;
+		MenuService.getMenuItemsByShortName($ctrl.user.favDishName)
+		.then(function(response){
+			if(!RegUserInfoService.checkInfo($ctrl.user.email)){
+				$ctrl.user.menuItem=response.data;
+				RegUserInfoService.savePreferences($ctrl.user);
+				$ctrl.itemFound=true;
+				//$ctrl.user={};
+				//$ctrl.itemFetchCalled=false;
+			}
+			else{
+				alert("This email Id is already been registered.");
+			}
+		},function(error){
+			$ctrl.itemFound=false;
+		});
 	}
 }
 })();
